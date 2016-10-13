9d8e971c916a82129f3070934df97963d104a58a
Sergey Komisarchik <komisarchik@media-saturn.com>
2016-10-13 13:04:47 +0300

    migrate to project.json

--- test/Serilog.Sinks.EventLog.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Serilog.Sinks.EventLog.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCopyright("Copyright © Serilog Contributors 2014")]
